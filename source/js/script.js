12947471565f593239e4fb1e1fe84c063d10ef87
codefornia <codeforniadreamin@gmail.com>
2021-05-13 20:06:21 +0300

    Добавлена базовая размека формы

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,23 @@
 const nav = document.querySelector ('.main-nav__list');
 const buttonNav = document.querySelector ('.main-nav__toggle');
+const errorPopup = document.querySelector(".popup--error");
+const reviewPopup = document.querySelector(".popup--review");
+const buttonErrorPopup = document.querySelector(".popup__button--error");
+const buttonReviewPopup = document.querySelector(".popup__button--review");
+const buttonReview = document.querySelector(".form__button");
 
 buttonNav.addEventListener('click', function(evt){
   evt.preventDefault();
   buttonNav.classList.toggle('main-nav__toggle--close');
   nav.classList.toggle('main-nav__list--close');
+});
+
+buttonReview.addEventListener('click', function(evt){
+  evt.preventDefault();
+  reviewPopup.classList.add('popup--open');
+});
+
+buttonReviewPopup.addEventListener('click', function(evt){
+  evt.preventDefault();
+  reviewPopup.classList.remove('popup--open');
 });
