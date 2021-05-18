94dcdb2e9e2102e378917d90d79579f00c89634e
codefornia <codeforniadreamin@gmail.com>
2021-05-18 16:01:34 +0300

    Пиксель перфект всех страниц

--- source/js/script.js
+++ source/js/script.js
@@ -1,23 +1,45 @@
-const nav = document.querySelector ('.main-nav__list');
-const buttonNav = document.querySelector ('.main-nav__toggle');
-const errorPopup = document.querySelector(".popup--error");
-const reviewPopup = document.querySelector(".popup--review");
-const buttonErrorPopup = document.querySelector(".popup__button--error");
-const buttonReviewPopup = document.querySelector(".popup__button--review");
-const buttonReview = document.querySelector(".form__button");
+const nav = document.querySelector('.main-nav__list');
+const buttonNav = document.querySelector('.main-nav__toggle');
+const errorPopup = document.querySelector('.popup--error');
+const reviewPopup = document.querySelector('.popup--review');
+const buttonErrorPopup = errorPopup.querySelector('.popup__button--error');
+const buttonReviewPopup = reviewPopup.querySelector('.popup__button--review');
+const buttonReview = document.querySelector('.form__button');
+const reviewForm = document.querySelector('.form');
+const nameInput = document.getElementById('bio-name');
+const surnameInput = document.getElementById('bio-surname');
+const telInput = document.getElementById('user-tel');
+const emailInput = document.getElementById('user-email');
 
-buttonNav.addEventListener('click', function(evt){
+buttonNav.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    buttonNav.classList.toggle('main-nav__toggle--close');
+    nav.classList.toggle('main-nav__list--close');
+  }
+);
+
+buttonReview.addEventListener('click', function (evt) {
   evt.preventDefault();
-  buttonNav.classList.toggle('main-nav__toggle--close');
-  nav.classList.toggle('main-nav__list--close');
-});
 
-buttonReview.addEventListener('click', function(evt){
-  evt.preventDefault();
-  reviewPopup.classList.add('popup--open');
-});
+  if (nameInput.checkValidity() &&
+    surnameInput.checkValidity() &&
+    telInput.checkValidity() &&
+    emailInput.checkValidity()) {
+    reviewPopup.classList.add('popup--open');
+    reviewForm.submit();
+    console.log('good');
+  } else {
+  errorPopup.classList.add('popup--open');
+  console.log('bad');
+  }
+})
 
-buttonReviewPopup.addEventListener('click', function(evt){
+buttonReviewPopup.addEventListener('click', function (evt) {
   evt.preventDefault();
   reviewPopup.classList.remove('popup--open');
-});
+})
+
+buttonErrorPopup.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  errorPopup.classList.remove('popup--open');
+})
