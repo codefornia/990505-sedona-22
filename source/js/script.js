b0a5a4ebb19efacdeb258d7c880a99e29b33042b
codefornia <codeforniadreamin@gmail.com>
2021-05-21 19:20:35 +0300

    правки Пиксель Перфект

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,9 @@
 const nav = document.querySelector('.main-nav__list');
 const buttonNav = document.querySelector('.main-nav__toggle');
-
-buttonNav.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    buttonNav.classList.toggle('main-nav__toggle--close');
-    nav.classList.toggle('main-nav__list--close');
-  }
-);
-
 const errorPopup = document.querySelector('.popup--error');
 const reviewPopup = document.querySelector('.popup--review');
-const buttonErrorPopup = errorPopup.querySelector('.popup__button--error');
-const buttonReviewPopup = reviewPopup.querySelector('.popup__button--review');
+const buttonErrorPopup = document.querySelector('.popup__button--error');
+const buttonReviewPopup = document.querySelector('.popup__button--review');
 const buttonReview = document.querySelector('.form__button');
 const reviewForm = document.querySelector('.form');
 const nameInput = document.getElementById('bio-name');
@@ -19,7 +11,16 @@ const surnameInput = document.getElementById('bio-surname');
 const telInput = document.getElementById('user-tel');
 const emailInput = document.getElementById('user-email');
 
-buttonReview.addEventListener('click', function (evt) {
+document.querySelector('.main-nav__list--no-js').classList.remove('main-nav__list--no-js');
+
+buttonNav?.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    buttonNav.classList.toggle('main-nav__toggle--close');
+    nav.classList.toggle('main-nav__list--close');
+  }
+);
+
+buttonReview?.addEventListener('click', function (evt) {
   evt.preventDefault();
 
   if (nameInput.checkValidity() &&
@@ -35,12 +36,35 @@ buttonReview.addEventListener('click', function (evt) {
   }
 })
 
-buttonReviewPopup.addEventListener('click', function (evt) {
+buttonReviewPopup?.addEventListener('click', function (evt) {
   evt.preventDefault();
   reviewPopup.classList.remove('popup--open');
 })
 
-buttonErrorPopup.addEventListener('click', function (evt) {
+buttonErrorPopup?.addEventListener('click', function (evt) {
   evt.preventDefault();
   errorPopup.classList.remove('popup--open');
 })
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (errorPopup.classList.contains('popup--open')) {
+      evt.preventDefault();
+      errorPopup.classList.remove('popup--open');
+      errorPopup.classList.remove('popup--open');
+      errorPopup.classList.remove('popup--open');
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (reviewPopup.classList.contains('popup--open')) {
+      evt.preventDefault();
+      reviewPopup.classList.remove('popup--open');
+      reviewPopup.classList.remove('popup--open');
+      reviewPopup.classList.remove('popup--open');
+    }
+  }
+});
